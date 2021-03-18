00f1beb7dd4a57051e11e272d2837203cd37a38f
mscarangella <59657676+mscarangella@users.noreply.github.com>
2021-03-18 14:30:10 -0400

    fix lab 8

--- lab8/index.js
+++ lab8/index.js
@@ -3,32 +3,37 @@ import fs from "fs";
 
 const App = Express();
 const port = 3010;
+
 //read database.json
 let fileContents = fs.readFileSync("database.json");
+
 //parse into json
 let database = JSON.parse(fileContents);
+
 //find employee name
 App.get("/api/employees/name/:name", (req, res) => {
   let result = { "error" : "Not Available"};
 
   database.forEach((value) => {
-    if(req.params.name == values.name){
+    if(req.params.name == value.name){
       result = value;
     }
   });
   res.json(result);
 });
+
 //find employee age
 App.get("/api/employees/ages/:number", (req, res) => {
   let result = { "error" : "Not Available"};
 
   database.forEach((value) => {
-    if(req.params.number == values.number){
+    if(req.params.number == value.number){
       result = value;
     }
   });
   res.json(result);
 })
+
 //port is running
 App.listen(port, () => {
   console.log("Server Running!");
