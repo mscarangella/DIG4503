805ad471f9ac213599befe7c3c75901bfb1147e1
mscarangella <59657676+mscarangella@users.noreply.github.com>
2021-04-07 17:32:00 -0400

    lab 11

---
+++ lab11/index.js
@@ -0,0 +1,21 @@
+import MongoClient from "mongodb";
+
+const URL = "mongodb+srv://mscarangella:lDTghULW7mCUe7cj@cluster0.oewwc.mongodb.net/test";
+
+MongoClient.connect(URL, {useUnifiedTopology: true})
+.then(connection => {
+  let database = connection.db("sample_airbnb");
+
+  let collection = database.collection("listingsAndReviews");
+
+  let cursor = collection.find({minimum_nights:{$eq:"2"}, beds: {$gte: "2"}, price: {$lt: "90"}});
+  
+  cursor.forEach(document =>{
+    console.log(document.name);
+  }, () => {
+    connection.close();
+  })
+})
+.catch(error => {
+  console.log("Error: " + error);
+});
